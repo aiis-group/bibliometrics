41c6a6bcfc52e9a928b56ea93847a11e0a862b1e
unknown <borjia98@gmail.com>
2019-03-06 08:31:26 +0000

    add researchGate scraper

--- scraper/researcher_gate_scraper.py
+++ scraper/researcher_gate_scraper.py
@@ -20,61 +20,58 @@ def get_stats(url):
     if not html:
         return None
 
-    stats_table = html.find('table', {'id': "gsc_rsb_st"})
-    stats_hist = html.find('div', {'class': "gsc_md_hist_b"})
+    about_section = html.find('div', {'id': "about"})
 
-    if stats_table:
-        stats_row = stats_table.tbody.findAll('tr')
-        cit = stats_row[0].findAll("td")
-        hIndex = stats_row[1].findAll("td")
-        i10 = stats_row[2].findAll("td")
-
-        stats = {
-            'citations': {'total': cit[1].get_text(),'last5Years': cit[2].get_text()},
-            'hIndex': {'total': hIndex[1].get_text(), 'last5Years': hIndex[2].get_text()},
-            'i10': {'total': i10[1].get_text(), 'last5Years': i10[2].get_text()}
-        }
-
-        if stats_hist:
-            last_year = int(stats_hist.findAll('span', {'class': 'gsc_g_t'})[-1].get_text()) + 1
-            citations = stats_hist.findAll('a', {'class': 'gsc_g_a'})
-
-            citations_per_year = {}
-
-            for a in citations:
-                z_index = int(a.get_attribute_list('style')[0].split(':')[-1])
-                citations_per_year[str(last_year - z_index)] = a.find("span").get_text()
-
-            stats["citationsPerYear"] = citations_per_year
-
-        return stats 
+    if about_section:
+        stats = html.findAll('div', {'class': "nova-e-text nova-e-text--size-xl nova-e-text--family-sans-serif nova-e-text--spacing-none nova-e-text--color-inherit"})
+        if stats:
+            stats = {
+                'researched_items': stats[0].get_text(),
+                'reads': stats[1].get_text(),
+                'citations': stats[2].get_text() 
+            }
+        return stats
 
     return None
 
 
 def get_personal_data(url):
     html = get_html(url)
+    personal_data = {}
 
     if not html:
         return None
 
-    personal_info = html.find('div', {'id': "gsc_prf_i"})
-    personal_data = None
+    about_section = html.find('div', {'id': "about"})
+
+    if not about_section:
+        return None
+    
+    personal_info = html.find('div', {'class': "header-inner vcard"})
     
     if (personal_info):
-        personal_info = personal_info.find('div', {'class':'gsc_prf_il'})
-        if personal_info:
-            personal_data = personal_info.get_text()
+        personal_info = personal_info.findAll('a')
+        personal_data['university'] = personal_info[0].get_text()
+        personal_data['department'] = personal_info[1].get_text()
 
-    study_fields = html.find('div', {'id': "gsc_prf_int"})
+    personal_info = html.find('div', {'class': "nova-o-grid__column nova-o-grid__column--width-4/12@xl-only nova-o-grid__column--width-5/12@m-up nova-o-grid__column--width-12/12@s-up"})
+
+    if (personal_info): 
+        personal_info = personal_info.find('div', {'class': "nova-e-list__item nova-v-institution-item__info-section-list-item"})
+        if personal_info:
+            personal_data['position'] = personal_info.get_text()
+
+    study_fields = html.findAll('a', {'class': "nova-e-badge nova-e-badge--color-grey nova-e-badge--display-block nova-e-badge--luminosity-medium nova-e-badge--size-l nova-e-badge--theme-ghost nova-e-badge--radius-full"})
 
     if (study_fields):
-        study_fields = [study_field.get_text() for study_field in study_fields.findAll('a')]
+        study_fields = [study_field.get_text() for study_field in study_fields]
+
+    if bool(personal_data) or study_fields:
         return {
             'personal_info': personal_data,
             'study_fields': study_fields
         }
-
+    
     return None
 
 
