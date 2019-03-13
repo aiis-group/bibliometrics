494ac111c5de4866ef516b78693d625b4ad65859
alexvj <alexvj@protonmail.com>
2019-03-13 13:35:07 +0000

    fixed profile url validators :hammer:

--- utils/validation_utils.py
+++ utils/validation_utils.py
@@ -3,10 +3,10 @@ import re
 def is_valid_rg_profile_url(url):
     """ Valid url example:  https://www.researchgate.net/profile/Javier_Sanchez11
     :return: True or False if the url is valid or not, respectively. """
-    return re.match('https://www.researchgate.net/profile/[^ /]+', url)
+    return re.match('https://www\.researchgate\.net/profile/[^ /]+', url)
 
 
 def is_valid_scholar_profile_url(url):
     """ Valid url example: https://scholar.google.es/citations?user=E6pJ7VAAAAAJ&hl=es
     :return: True or False if the url is valid or not, respectively. """
-    return re.match('^https://scholar.google.es/citations\?user=.*', url)
+    return re.match('^https://scholar\.google\..{2,4}/citations\?.*user=.*', url)
