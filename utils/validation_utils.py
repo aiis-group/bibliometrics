ea00dd5270db48a2906746e4c1750068b329e768
unknown <borjia98@gmail.com>
2019-03-13 09:11:02 +0000

    validate scholar url

--- utils/validation_utils.py
+++ utils/validation_utils.py
@@ -7,5 +7,6 @@ def is_valid_rg_profile_url(url):
 
 
 def is_valid_scholar_profile_url(url):
-    """TODO"""
-    return True
+    """ Valid url example: https://scholar.google.es/citations?user=E6pJ7VAAAAAJ&hl=es
+    :return: True or False if the url is valid or not, respectively. """
+    return re.match('^https://scholar.google.es/citations\?user=.*', url)
